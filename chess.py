0e3da1b5e09132e9e35ac52218bce7713f95d273
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-07-03 21:47:42 +0530

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,51 @@
+#---------------------------------------------------------------------------------------------------------------------------------
+### Authenticator
+#---------------------------------------------------------------------------------------------------------------------------------
+import streamlit as st
+#---------------------------------------------------------------------------------------------------------------------------------
+### Template Graphics
+#---------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
+### Import Libraries
+#---------------------------------------------------------------------------------------------------------------------------------
+import chess
+
+#---------------------------------------------------------------------------------------------------------------------------------
+### Title and description for your Streamlit app
+#---------------------------------------------------------------------------------------------------------------------------------
+#import custom_style()
+st.set_page_config(page_title="♟️ Fun with Chess",
+                   layout="wide",
+                   #page_icon=               
+                   initial_sidebar_state="collapsed")
+#----------------------------------------
+st.title(f""":rainbow[:chess_pawn:Fun with Chess | v0.1]""")
+st.markdown('Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
+            unsafe_allow_html=True)
+#st.info('**Disclaimer : :blue[Thank you for visiting the app] | Unauthorized uses or copying of the app is strictly prohibited | Click the :blue[sidebar] to follow the instructions to start the applications.**', icon="ℹ️")
+#----------------------------------------
+# Set the background image
+st.divider()
+
+#---------------------------------------------------------------------------------------------------------------------------------
+### Functions & Definitions
+#---------------------------------------------------------------------------------------------------------------------------------
+
+board = chess.Board()
+
+board_ui = st.empty()
+board_ui.write(board.unicode(invert_color=True, empty_square="·"))
+
+user_move = st.text_input("Enter your move (e.g. e2e4):")
+
+if user_move:
+    try:
+        board.push_uci(user_move)
+        board_ui.write(board.unicode(invert_color=True, empty_square="·"))
+    except ValueError:
+        st.error("Invalid move!")
+
+if st.button("Reset Board"):
+    board.reset()
+    board_ui.write(board.unicode(invert_color=True, empty_square="·"))
+
